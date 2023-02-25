507f14881449593743fa2e44ee77f1784fa8889c
Loris Leiva <loris.leiva@gmail.com>
2023-02-25 18:42:50 +0000

    Use custom serializers for tx messages

--- packages/umi-transaction-factory-web3js/src/Web3JsTransactionFactory.ts
+++ packages/umi-transaction-factory-web3js/src/Web3JsTransactionFactory.ts
@@ -1,33 +1,43 @@
+/* eslint-disable no-bitwise */
 import {
+  base58,
+  CompiledAddressLookupTable,
+  CompiledInstruction,
+  Context,
   SerializedTransaction,
   SerializedTransactionMessage,
+  Serializer,
   Transaction,
   TransactionFactoryInterface,
   TransactionInput,
   TransactionMessage,
+  TransactionMessageHeader,
 } from '@metaplex-foundation/umi-core';
 import {
   fromWeb3JsMessage,
   fromWeb3JsTransaction,
-  toWeb3JsMessage,
   toWeb3JsMessageFromInput,
   toWeb3JsTransaction,
 } from '@metaplex-foundation/umi-web3js-adapters';
-import {
-  VersionedTransaction as Web3JsTransaction,
-  VersionedMessage as Web3JsMessage,
-} from '@solana/web3.js';
+import { VersionedTransaction as Web3JsTransaction } from '@solana/web3.js';
 
 export class Web3JsTransactionFactory implements TransactionFactoryInterface {
+  protected readonly context: Pick<Context, 'serializer'>;
+
+  constructor(context: Web3JsTransactionFactory['context']) {
+    this.context = context;
+  }
+
   create(input: TransactionInput): Transaction {
     const web3JsMessage = toWeb3JsMessageFromInput(input);
+    const message = fromWeb3JsMessage(web3JsMessage);
     const web3JsTransaction = new Web3JsTransaction(
       web3JsMessage,
       input.signatures
     );
     return {
-      message: fromWeb3JsMessage(web3JsMessage),
-      serializedMessage: web3JsMessage.serialize(),
+      message,
+      serializedMessage: this.serializeMessage(message),
       signatures: web3JsTransaction.signatures,
     };
   }
@@ -43,12 +53,104 @@ export class Web3JsTransactionFactory implements TransactionFactoryInterface {
   }
 
   serializeMessage(message: TransactionMessage): SerializedTransactionMessage {
-    return new Uint8Array(toWeb3JsMessage(message).serialize());
+    return this.getTransactionMessageSerializer().serialize(message);
   }
 
   deserializeMessage(
     serializedMessage: SerializedTransactionMessage
   ): TransactionMessage {
-    return fromWeb3JsMessage(Web3JsMessage.deserialize(serializedMessage));
+    return this.getTransactionMessageSerializer().deserialize(
+      serializedMessage
+    )[0];
+  }
+
+  getTransactionMessageSerializer(): Serializer<TransactionMessage> {
+    const s = this.context.serializer;
+    return s.struct<TransactionMessage, TransactionMessage>([
+      // const MESSAGE_VERSION_0_PREFIX = 1 << 7;
+      ['version', s.u8()], // TODO
+      [
+        'header',
+        s.struct<TransactionMessageHeader, TransactionMessageHeader>([
+          ['numRequiredSignatures', s.u8()],
+          ['numReadonlySignedAccounts', s.u8()],
+          ['numReadonlyUnsignedAccounts', s.u8()],
+        ]),
+      ],
+      ['accounts', s.array(s.publicKey(), { size: shortU16() })],
+      ['blockhash', s.string({ encoding: base58, size: 32 })],
+      [
+        'instructions',
+        s.array(this.getCompiledInstructionSerializer(), { size: shortU16() }),
+      ],
+      [
+        'addressLookupTables',
+        s.array(this.getCompiledAddressLookupTableSerializer(), {
+          size: shortU16(),
+        }),
+      ],
+    ]);
+  }
+
+  getCompiledInstructionSerializer(): Serializer<CompiledInstruction> {
+    const s = this.context.serializer;
+    return s.struct<CompiledInstruction>([
+      ['programIndex', s.u8()],
+      ['accountIndexes', s.array(s.u8(), { size: shortU16() })],
+      ['data', s.bytes({ size: shortU16() })],
+    ]);
+  }
+
+  getCompiledAddressLookupTableSerializer(): Serializer<CompiledAddressLookupTable> {
+    const s = this.context.serializer;
+    return s.struct<CompiledAddressLookupTable>([
+      ['publicKey', s.publicKey()],
+      ['writableIndexes', s.array(s.u8(), { size: shortU16() })],
+      ['readonlyIndexes', s.array(s.u8(), { size: shortU16() })],
+    ]);
   }
 }
+
+/**
+ * Same as u16, but serialized with 1 to 3 bytes.
+ *
+ * If the value is above 0x7f, the top bit is set and the remaining
+ * value is stored in the next bytes. Each byte follows the same
+ * pattern until the 3rd byte. The 3rd byte, if needed, uses
+ * all 8 bits to store the last byte of the original value.
+ */
+export const shortU16 = (): Serializer<number> => ({
+  description: 'shortU16',
+  fixedSize: null,
+  maxSize: 3,
+  serialize: (value: number): Uint8Array => {
+    const bytes = [] as number[];
+    let remainingValue = value;
+    for (;;) {
+      let elem = remainingValue & 0x7f;
+      remainingValue >>= 7;
+      if (remainingValue === 0) {
+        bytes.push(elem);
+        break;
+      } else {
+        elem |= 0x80;
+        bytes.push(elem);
+      }
+    }
+    return new Uint8Array(bytes);
+  },
+  deserialize: (buffer: Uint8Array, offset = 0): [number, number] => {
+    const bytes = [...buffer.slice(offset)];
+    let len = 0;
+    let size = 0;
+    for (;;) {
+      const elem = bytes.shift() as number;
+      len |= (elem & 0x7f) << (size * 7);
+      size += 1;
+      if ((elem & 0x80) === 0) {
+        break;
+      }
+    }
+    return [len, offset + size];
+  },
+});
