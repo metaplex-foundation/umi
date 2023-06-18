892e1e7872e199c4f36e6d38b0d85a838d3ff46a
Loris Leiva <loris.leiva@gmail.com>
2023-06-18 16:24:37 +0100

    Cleanup unused test helpers

--- packages/umi-serializers-numbers/test/_setup.ts
+++ packages/umi-serializers-numbers/test/_setup.ts
@@ -36,36 +36,6 @@ export const assertRangeError = <T>(
   });
 };
 
-/** Assert serialization using a hex string. */
-export const s = <T, U extends T = T>(
-  t: Assertions,
-  serializer: Serializer<T, U>,
-  value: T extends T ? T : never,
-  expected: string
-): void => {
-  const [actual] = base16.deserialize(serializer.serialize(value));
-  t.is(actual, expected);
-};
-
-/** Assert deserialization using a hex string. */
-export const d = <T, U extends T = T>(
-  t: Assertions,
-  serializer: Serializer<T, U>,
-  hexBytes: string | [string, number],
-  expected: U,
-  expectedOffset?: number
-): void => {
-  const [resolvedHexBytes, offset] = Array.isArray(hexBytes)
-    ? hexBytes
-    : [hexBytes, undefined];
-  const bytes = base16.serialize(resolvedHexBytes);
-  const [actual, newOffset] = serializer.deserialize(bytes, offset);
-  t.deepEqual(actual, expected);
-  if (expectedOffset !== undefined) {
-    t.is(newOffset, expectedOffset);
-  }
-};
-
 export const base16: Serializer<string> = {
   description: 'base16',
   fixedSize: null,
