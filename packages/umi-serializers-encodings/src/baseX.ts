0b35dc2613824b27c1d37d05513b2107a5457985
Loris Leiva <loris.leiva@gmail.com>
2023-08-25 13:56:07 +0100

    Create baseXReslice serializer and use it for base64

--- packages/umi-serializers-encodings/src/baseX.ts
+++ packages/umi-serializers-encodings/src/baseX.ts
@@ -2,8 +2,14 @@ import type { Serializer } from '@metaplex-foundation/umi-serializers-core';
 import { InvalidBaseStringError } from './errors';
 
 /**
- * A string serializer that uses a custom alphabet.
- * This can be used to create serializers for base58, base64, etc.
+ * A string serializer that requires a custom alphabet and uses
+ * the length of that alphabet as the base. It then divides
+ * the input by the base as many times as necessary to get
+ * the output. It also supports leading zeroes by using the
+ * first character of the alphabet as the zero character.
+ *
+ * This can be used to create serializers such as base10 or base58.
+ *
  * @category Serializers
  */
 export const baseX = (alphabet: string): Serializer<string> => {
