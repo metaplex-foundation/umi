e9fbbebf94c7a9b5f8d4370801a7a23c481aa5dc
Tony Boyle <81017245+tonyboylehub@users.noreply.github.com>
2024-09-27 09:27:57 +0700

    irys is moving to own L1 so should update the gateway to reflect that to stop some potential broken links which I think mainly apply to devnet currently, but will most likely effect mainnet in the near future too

--- packages/umi-uploader-irys/src/createIrysUploader.ts
+++ packages/umi-uploader-irys/src/createIrysUploader.ts
@@ -149,7 +149,7 @@ export function createIrysUploader(
         throw new AssetUploadFailedError(status);
       }
 
-      return `https://arweave.net/${data.id}`;
+      return `https://gateway.iryz.xyz/${data.id}`;
     });
 
     return Promise.all(promises);
